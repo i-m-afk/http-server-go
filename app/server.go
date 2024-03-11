f4da17eb4f2473da418f7230b439dd297486dab6
Rishav Kumar <rishavkumar208@gmail.com>
2024-03-12 01:39:42 +0530

    404 response

--- app/server.go
+++ app/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,9 +15,27 @@ func main() {
 		os.Exit(1)
 	}
 	conn, err := l.Accept()
+	buffer := make([]byte, 100)
+	n, err := conn.Read(buffer)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(buffer[:n]))
+	splits := strings.Split(string(buffer[:n]), "\r\n")
+	startLine := splits[0]
+	startLineSplits := strings.Split(startLine, " ")
+	// path :
+	path := startLineSplits[1]
+	fmt.Println(path)
 
 	// reply
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
